767046c3a1e1293215b9873e1aa5b9b57f574c00
Alberto Rosa <alberto.r.93@hotmail.it>
2022-04-08 09:09:00 +0200

    Add project files.

---
+++ csharp-geometria/Classi.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_geometria
+{
+    public class Rettangolo
+    {
+        private int laBase;
+        private int altezza;
+
+
+        //costruttore
+        public Rettangolo(int laBase, int altezza)
+        {
+            this.laBase = laBase;
+            this.altezza = altezza;
+        }
+
+
+        //calcolaArea
+        private int Area()
+        {
+            return laBase * altezza;
+        }
+
+
+        //calcolaPerimetro
+
+        private int Perimetro()
+        {
+            return (laBase + altezza) * 2;
+        }
+
+
+        //visualizza risultati
+        public void Risultato()
+        {
+            Console.WriteLine("La base misura " + laBase + "cm. L'altezza misura " + altezza + "cm.");
+            Console.WriteLine("L'area è di " + Area() + "cm quadri e il perimetro è di " + Perimetro() + "cm");
+        }
+
+
+
+
+
+    }
+
+}
+
+
+
+/*
+ * 1. Creare una classe Rettangolo con due attributi interi: baseRettangolo e altezzaRettangolo.
+Aggiungere un opportuno costruttore con parametri.
+Aggiungere due metodi: calcolaArea e calcolaPerimetro che calcolano
+e restituiscono, rispettivamente, l’area e il perimetro del rettangolo.
+*/
---
+++ csharp-geometria/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using csharp_geometria;
+
+/*
+2. Poi, nel vostro programma principale (Program.cs) chiedete all’utente di inserire, da console,
+i valori di base e di altezza con cui istanziare un nuovo Rettangolo.
+Dopo averlo istanziato, stampate a video il perimetro e l’area.
+
+3. Provate ad istanziare tanti rettangoli con nome diverso e attributi diversi
+(baseRettangolo e altezzaRettangolo) e provate a stampare le loro proprietà ad
+esempio con un metodo StampaRettangolo che vi stampi il vostro rettangolo ad esempio cosi:
+
+ —— Rettangolo1 ——
+base: 20 cm
+altezza: 10 cm
+Perimetro: 60 cm
+Area: 200 cm2
+*/
+
+Console.WriteLine("Creiamo un quadrato");
+
+Console.Write("Quanti cm misura la base? ");
+int laBase = Int32.Parse(Console.ReadLine());
+Console.Write("QUanti cm misura l'altezza? ");
+int altezza = Int32.Parse(Console.ReadLine());
+
+Rettangolo primoRettangolo = new Rettangolo(laBase, altezza);
+
+primoRettangolo.Risultato();
+
+
